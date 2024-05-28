291a55b47afd5e0b1960d4b513dc429ee5261353
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-05-28 16:39:45 +0530

    Add component order/ product/ customer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {SearchComponent} from "./search/search.component";
+import { CustomerComponent } from './components/customer/customer.component';
+import { ProductComponent } from './components/product/product.component';
+import { OrderComponent } from './components/order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    CustomerComponent,
+    ProductComponent,
+    OrderComponent,
+
   ],
   imports: [
     BrowserModule,
