1bbe2db21573153724e3f7e7ece5410232c61776
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-05-29 10:30:14 +0530

    routing part set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CustomerComponent} from "./components/customer/customer.component";
+import {OrderComponent} from "./components/order/order.component";
+import {ProductComponent} from "./components/product/product.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'customer',component:CustomerComponent},
+  {path:'order',component:OrderComponent},
+  {path:'product',component:ProductComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
